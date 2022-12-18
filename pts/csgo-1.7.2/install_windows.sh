fad5d95e83cdc5929fb278f675593b7c2055508e
Thaddeus C <thaddeusc1@users.noreply.github.com>
2022-12-18 10:52:22 -0500

    - Validate Steam installation status and location via Windows Registry.- Update CS:GO game install and replay file install using Steam configuration data from Windows Registry.

--- pts/csgo-1.7.2/install_windows.sh
+++ pts/csgo-1.7.2/install_windows.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
-steam steam://install/730
+if /cygdrive/c/windows/system32/reg.exe query "HKEY_CURRENT_USER\Software\Valve\Steam" /v SteamPath
+then
+    STEAMPATH=`/cygdrive/c/windows/system32/reg.exe query "HKEY_CURRENT_USER\Software\Valve\Steam" /v SteamPath | awk '$1 ~ "SteamPath" && $2 ~ "REG_SZ" { print(substr($3, 1, length($3)-1)) }'`
+else
+    echo "ERROR: Steam is not found on the system! This test profile needs to find Steam configuration data in the Windows Registry."
+	echo 2 > ~/install-exit-status
+    exit
+fi
 
+/cygdrive/${STEAMPATH:0:1}/${STEAMPATH:3}/steam.exe "steam://install/730"
 unzip -o csgo-demo-10.zip
-mv pts10.dem "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo"
+mv pts10.dem "${STEAMPATH}/steamapps/common/Counter-Strike Global Offensive/csgo"
 
 echo "#!/bin/sh
 cd \"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\"
@@ -14,3 +22,4 @@ cat csgo/log.log* > \$LOG_FILE
 cat csgo/SourceBench* >> \$LOG_FILE
 cat csgo/UNKNOWN >> \$LOG_FILE" > csgo
 chmod +x csgo
+echo 0 > ~/install-exit-status
